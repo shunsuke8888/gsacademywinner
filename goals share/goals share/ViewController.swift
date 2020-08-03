6a53463dc9196a5315f1a2d2eb436ff45cbdb9e3
村松 俊輔 <shunsuke.m8888@gmail.com>
2020-08-04 04:39:10 +0900

    初期設定

---
+++ goals share/goals share/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goals share
+//
+//  Created by 村松 俊輔 on 2020/08/04.
+//  Copyright © 2020 Shunsuke Muramatsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
